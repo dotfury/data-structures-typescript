a1ea0e16606984f5b507b436f2a8b607fb70a419
dotfury <cmfurious@dotfury.com>
2019-11-25 15:20:34 +0900

    chore(doubly linked list): test for push method

--- src/classes/DoublyLinkedList/__tests__/index.ts
+++ src/classes/DoublyLinkedList/__tests__/index.ts
@@ -1,10 +1,25 @@
 import DoublyLinkedList from '../';
 
-describe('Can create a doubly linked list', () => {
+describe('====DOUBLY LINKED LIST====', () => {
   test('Can create an empty list', () => {
     const list = new DoublyLinkedList();
 
     expect(list.head).toBe(null);
     expect(list.tail).toBe(null);
+  });
+
+  test('Can push node to end of list', () => {
+    const list = new DoublyLinkedList();
+
+    list.push(1);
+    list.push(2);
+
+    expect(list.length).toBe(2);
+    expect(list.head!.value).toBe(1);
+    expect(list.tail!.value).toBe(2);
+    expect(list.head!.previous).toBe(null);
+    expect(list.head!.next!.value).toBe(2);
+    expect(list.tail!.previous!.value).toBe(1);
+    expect(list.tail!.next).toBe(null);
   });
 });

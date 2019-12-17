b20e9f0919d6f199a191ea8abad1e8d383f7ed0f
dotfury <cmfurious@dotfury.com>
2019-12-17 12:00:25 +0900

    feat(max binary heap): add extract max method

--- src/classes/MaxBinaryHeap/__tests__/index.ts
+++ src/classes/MaxBinaryHeap/__tests__/index.ts
@@ -51,4 +51,41 @@ describe('====MAX BINARY HEAP====', () => {
     expect(heap.values.length).toBe(7);
     expect(heap.values).toEqual([55, 39, 41, 18, 27, 12, 33]);
   });
+
+  test('Can extract max value', () => {
+    const heap = new MaxBinaryHeap();
+
+    heap.insert(41);
+    heap.insert(39);
+    heap.insert(33);
+    heap.insert(18);
+    heap.insert(27);
+    heap.insert(12);
+    heap.insert(55);
+
+    const removed = heap.extractMax();
+
+    expect(heap.values.length).toBe(6);
+    expect(removed).toBe(55);
+    expect(heap.values).toEqual([41, 39, 33, 18, 27, 12]);
+
+    const removedSecond = heap.extractMax();
+
+    expect(heap.values.length).toBe(5);
+    expect(removedSecond).toBe(41);
+    expect(heap.values).toEqual([39, 27, 33, 18, 12]);
+  });
+
+  test('Can extract max value last item', () => {
+    const heap = new MaxBinaryHeap();
+
+    heap.insert(41);
+
+    const removed = heap.extractMax();
+    const removedSecond = heap.extractMax();
+
+    expect(heap.values.length).toBe(0);
+    expect(removed).toBe(41);
+    expect(removedSecond).toBe(undefined);
+  });
 });
--- src/classes/MaxBinaryHeap/index.ts
+++ src/classes/MaxBinaryHeap/index.ts
@@ -23,4 +23,47 @@ export default class MaxBinaryHeap {
       parentIndex = Math.floor((index - 1) / 2);
     }
   }
+
+  extractMax(): ValueType | undefined {
+    const returnValue = this.values[0];
+    const end = this.values.pop();
+    
+    if (this.values.length > 0) {
+      this.values[0] = end!;
+      this.sinkDown();
+    }
+
+    return returnValue;
+  }
+
+  sinkDown(): void {
+    const length = this.values.length;
+    let index = 0;
+
+    while (true) {
+      const leftChildIndex = (2 * index) + 1;
+      const rightChildIndex = (2 * index) + 2;
+      const leftChild = (leftChildIndex < length) ? this.values[leftChildIndex] : null;
+      const rightChild = (rightChildIndex < length) ? this.values[rightChildIndex] : null;
+      const element = this.values[index];
+      let swapIndex = null;
+
+      if (leftChild && leftChild > element) {
+        swapIndex = leftChildIndex;
+      }
+
+      if ((rightChild && rightChild > element) && swapIndex === null ||
+        leftChild && (rightChild && rightChild > leftChild)) {
+        swapIndex = rightChildIndex;
+      }
+
+      if (swapIndex === null) {
+        break;
+      }
+
+      this.values[index] = this.values[swapIndex];
+      this.values[swapIndex] = element;
+      index = swapIndex;
+    }
+  }
 }

d3540d93cd33d0b251c0bc6d3ecaa2114df28aea
charles montgomery <cmfurious@dotfury.com>
2020-01-11 16:38:26 +0900

    feat(graph): add depth first search and breadth first search

--- src/classes/Graph/index.ts
+++ src/classes/Graph/index.ts
@@ -1,3 +1,7 @@
+interface IVisitedObject {
+  [key: string]: boolean;
+}
+
 export default class Graph {
   adjacencyList: {};
 
@@ -31,5 +35,87 @@ export default class Graph {
 
       delete this.adjacencyList[vertex];
     }
+  }
+
+  dfsr(vertex: string): string[] | null {
+    const results: string[] = [];
+    const visited: IVisitedObject = {};
+    const adjacencyList = this.adjacencyList;
+
+    function helper(vertex: string) {
+      if (!adjacencyList[vertex]) {
+        return null;
+      }
+
+      results.push(vertex);
+      visited[vertex] = true;
+
+      adjacencyList[vertex].forEach((item: string) => {
+        if (!visited[item]) {
+          return helper(item);
+        }
+
+        return;
+      });
+    }
+
+    helper(vertex);
+
+    return results;
+  }
+
+  dfsi(vertex: string): string[] | null {
+    const results: string[] = [];
+    const visited: IVisitedObject = {};
+    const stack: any[] = [];
+
+    if (!this.adjacencyList[vertex]) {
+      return null;
+    }
+
+    stack.push(vertex);
+
+    while (stack.length > 0) {
+      const item = stack.pop();
+
+      if (!visited[item]) {
+        visited[item] = true;
+        results.push(item);
+
+        this.adjacencyList[item].forEach((neighbor: string) => {
+          stack.push(neighbor);
+        });
+      }
+    }
+
+    return results;
+  }
+
+  bfs(vertex: string): string[] | null {
+    const queue: any[] = [];
+    const results: string[] = [];
+    const visited: IVisitedObject = {};
+
+    if (!this.adjacencyList[vertex]) {
+      return null;
+    }
+
+    queue.push(vertex);
+    visited[vertex] = true;
+
+    while (queue.length > 0) {
+      const item = queue.shift();
+
+      results.push(item);
+
+      this.adjacencyList[item].forEach((neighbor: string) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return results;
   }
 }
